fb440b3f0b8c3e2f98403413967c41d880738bef
David Cuevas <davidjfcpage@gmail.com>
2014-04-09 01:30:36 +0100

    Update das match views e URLs

--- gba/match/models.py
+++ gba/match/models.py
@@ -1,3 +1,7 @@
+from django.db.models.fields import CharField
 from django.db import models
 
 # Create your models here.
+
+class Match(models.Model):
+    competition_name = CharField(max_length=20)
--- gba/match/views.py
+++ gba/match/views.py
@@ -1,9 +1,9 @@
 from django.shortcuts import render
-from models import Match
+from match.models import Match
 
 def index(request):
-    competitions = Match.objects.all()
-    context = RequestContext(request,
-                             {'allMatches': matches}
-    )
-    return render(request, 'match/index.html', context)
+    # competitions = Match.objects.all()
+    # context = RequestContext(request,
+    #                         {'allMatches': matches}
+    # )
+    return render(request, 'match/index.html')
