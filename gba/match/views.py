7a9294dd1230525798dce3fe79407337f4d9b426
David Cuevas <davidjfcpage@gmail.com>
2014-04-10 12:09:39 +0100

    Updated Match View

--- gba/match/views.py
+++ gba/match/views.py
@@ -1,9 +1,8 @@
 from django.shortcuts import render
 from match.models import Match
+from django.template.context import RequestContext
 
 def index(request):
-    # competitions = Match.objects.all()
-    # context = RequestContext(request,
-    #                         {'allMatches': matches}
-    # )
-    return render(request, 'match/index.html')
+     matches = Match.objects.all()
+     context = RequestContext(request,{'allMatches': matches})
+     return render(request, 'match/index.html', context)
