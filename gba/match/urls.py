b367ec98cd7da2183e5b51fbe212451e52575364
David Cuevas <davidjfcpage@gmail.com>
2014-04-12 02:56:05 +0100

    Added Bootstrap; Added On-screen logic to delete objects;

--- gba/match/urls.py
+++ gba/match/urls.py
@@ -4,5 +4,6 @@ from match import views
 urlpatterns = patterns('',
                        url(r'^$', views.index, name='index'),
                        url(r'^New', views.new, name='new'),
-                       #url(r'^insert', views.insert, name='insert'),
+                       url(r'^Insert', views.insert, name='insert'),
+                       url(r'^Delete', views.delete, name='delete'),
 )
--- gba/match/views.py
+++ gba/match/views.py
@@ -3,7 +3,7 @@ from match.models import Match
 from team.models import Team
 from django.template.context import RequestContext
 from competition.models import Competition
-
+from django.http.response import HttpResponseRedirect
 
 def index(request):
      matches = Match.objects.all()
@@ -15,3 +15,19 @@ def new(request):
     competitions = Competition.objects.all()
     context = RequestContext(request, {'allTeams': teams, 'allCompetitions': competitions,})
     return render(request, 'match/new.html',context )
+
+def insert(request):
+    if request.method == 'POST': # If the form has been submitted...
+        postCompetition = Competition.objects.get(pk=request.POST["competition"])
+        postHomeTeam= Team.objects.get(pk=request.POST["homeTeam"])
+        postAwayTeam = Team.objects.get(pk=request.POST["awayTeam"])
+        m = Match(
+            competition = postCompetition, homeTeam = postHomeTeam, awayTeam = postAwayTeam)
+        m.save()
+    return HttpResponseRedirect('/Match/')
+
+def delete(request):
+    if request.method == 'POST': # If the form has been submitted...
+        m = Match.objects.get(pk=request.POST["match"])
+        m.delete()
+    return HttpResponseRedirect('/Match/')
